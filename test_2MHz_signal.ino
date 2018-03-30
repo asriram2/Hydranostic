058d920fd5d6e168537f5e4cb21bb4a81d6760e5
asriram2 <annapurni_sriram@hotmail.com>
2018-03-29 18:49:08 -0700

    Update test_2MHz_signal.ino

--- test_2MHz_signal.ino
+++ test_2MHz_signal.ino
@@ -1,10 +1,21 @@
-const int freqOutputPin = 3;  
+// BwB Hydration Monitor brought to you by #TeamAntiThirst
+// Updated: 03.29.2018 by A. Sriram
+// Authors: Caleb Perez and Annapurni Sriram
+// This program generates a 2 MHz signal through output pin 3 using the ATmega chip registers.
+// Code was adapted from: https://forum.arduino.cc/index.php?topic=65434.0 and http://forum.arduino.cc/index.php/topic,62964.0.html
+
+const int freqOutputPin = 3; 
+// The OCR2A pin is the counter which will cause the counter to reset when encountered. ocra2aval is the value later assigned to OCR2A.
+// This sets the frequency of the signal. Without pre-scaling, an OCRA2A value of 3 the output pin to toggle the value every four CPU 
+// clock cycles. That is, the period is equal to eight clock cycles.
+//
+// With F_CPU = 16 MHz, the result is 2 MHz.
 const int ocr2aval  = 3; 
 
 // These are not needed to generate the code.  They are simply to remind the user
 // what the frequency is.  Note that changing the value of "prescale" here does
 // not affect the counter operation.  It's just for the printout vlaue
-const int prescale  = 1;
+const int prescale  = 1; // no prescaling
 const float period    = 2.0 * prescale * (ocr2aval+1) / (F_CPU/1.0e6);
 const float freq      = 1.0e6 / period;
 
